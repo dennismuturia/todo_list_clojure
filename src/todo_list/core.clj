3c13007bf4b259271a19f6472d3db250856f961b
dennismuturia <dmuturia@mo-de.local>
2018-10-01 11:56:04 +0300

    Added a handler that will process the web services

--- src/todo_list/core.clj
+++ src/todo_list/core.clj
@@ -10,3 +10,15 @@
                   :body "<h2>Hello clojure</h2> <p>This is just a start to the place</p>"
                   :headers {}})
                   {:port (Integer. port-number)}))
+
+(defn welcome
+  "A handler to process the request sent to the web application"
+  [request]
+  (if ("/" (:uri request))
+  {:status 200
+  :body "<h1>Hello Clojure world </h1> <p> Welcome to my application </p>"
+  :headers {}}
+  {:status 404
+  :body "<h1>Sorry the page cant be found</h1>
+    <p>Page not found</p>"
+    :headers {}}))
