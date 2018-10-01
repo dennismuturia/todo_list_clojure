3210622b1f58a4ef047d89c888954624a6f95f9f
dennismuturia <dmuturia@mo-de.local>
2018-10-01 09:56:44 +0300

    This is used to run a basic server

--- src/todo_list/core.clj
+++ src/todo_list/core.clj
@@ -2,4 +2,12 @@
 (ns todo-list.core
   (:require [ring.adapter.jetty :as jetty])
 
-
+;;Include the main that will contain to run Jetty and Ring
+(defn -main
+  "The start of the ring web service"
+  [port-number]
+  (jetty/run-jetty
+    (fn [request] {:status 200
+                  :body "<h2>Hello clojure</h2>"
+                  :headers {}})
+                  {:port (Integer. port-number)}))
