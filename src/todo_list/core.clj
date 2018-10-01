cec84aa05c431c3b6369e8bfb92f3ac55a75da9f
dennismuturia <dmuturia@mo-de.local>
2018-10-01 17:06:59 +0300

    Successfully incorporated the routes in the site using compojure

--- src/todo_list/core.clj
+++ src/todo_list/core.clj
@@ -1,33 +1,35 @@
 (ns todo-list.core
   (:require [ring.adapter.jetty :as jetty]
   [ring.middleware.reload :refer [wrap-reload]]
-  [compojure.core :refer :all]))
+  [compojure.core :refer :all]
+  [compojure.route :refer [not-found]]))
 
-;;Include the main that will contain to run Jetty and Ring
-(defn -main
-  "The start of the ring web service"
-  [port-number]
-  (jetty/run-jetty
-    (fn [request] {:status 200
-                  :body "<h2>Hello clojure</h2> <p>This is just a start to the place</p>"
-                  :headers {}})
-                  {:port (Integer. port-number)}))
 
 (defn welcome
   "A handler to process the request sent to the web application"
   [request]
-  (if (= "/" (:uri request))
   {:status 200
-  :body "<h1>Hello Clojure world </h1> <p> Welcome to my application </p> <h5> Heeyyy</h5>"
-  :headers {}}
-  {:status 404
-  :body "<h1>Sorry the page cant be found</h1>
-    <p>Page not found</p>"
-    :headers {}}))
+  :body "<h1>Hello to thecompojure site</h2>"
+  :headers{}}
+)
+
+;;Create a route to the home
+(defroutes app
+  (GET "/" [] welcome)
+  (not-found "<h1>This is not you are looking for</h1><p>Am so sorry</p>")
+)
+
+(defn -main
+"The start of the webservice"
+  [port-number]
+  (jetty/run-jetty app
+    {:port (Integer. port-number)}
+  )
+)
 
 ;;This is only used in development
 (defn -dev-main
   [port-number]
   (jetty/run-jetty
-    (wrap-reload #'welcome)
+    (wrap-reload #'app)
     {:port (Integer. port-number)}))
