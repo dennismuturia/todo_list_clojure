6adda1b81705a8e8885b003ccd1e7d06cdb63596
dennismuturia <dmuturia@mo-de.local>
2018-10-02 13:02:17 +0300

    Deploying to heroku

--- project.clj
+++ project.clj
@@ -8,5 +8,7 @@
   [compojure "1.1.8"]
   [http-kit "2.1.16"]]
   :main todo-list.core
+  :min-lein-version "2.0.0"
+  :uberjar-name "todo-list.jar"
   :profiles {:dev {:main todo-list.core/-dev-main}}
   )
